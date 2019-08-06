3cda226a6fb8988929fef0d952f882df9677375e
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-06 18:05:50 -0400

    Bind com anotação

--- Models/AddressSummary.cs
+++ Models/AddressSummary.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ProAspNetCoreMvcModelBinding.Models
 {
+    [Bind(nameof(City))]
     public class AddressSummary
     {
         public string City { get; set; }
